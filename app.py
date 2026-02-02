b98e3a2e1ce68bff5761e07c48ef48e665cc79a1
JaydenTanJunRen <144462947+JaydenTanJunRen@users.noreply.github.com>
2026-02-02 14:30:14 +0800

    Create app.py

---
+++ app.py
@@ -0,0 +1,8 @@
+import streamlit as st
+
+st.set_page_config(
+    page_title="Landslide Guard AI",
+    page_icon="🌋",
+    layout="wide", # This makes it look like a dashboard
+    initial_sidebar_state="expanded"
+)
